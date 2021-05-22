e1eaa4bdba90f7778a5e07fee360b7af787b028b
fenris <Fenris-v>
2021-05-22 21:14:52 +0300

    adaptive

--- js/scripts.js
+++ js/scripts.js
@@ -9,21 +9,12 @@ $(document).ready(() => {
         slidesToShow: 2,
         autoplay: true,
         responsive: [
-            // {
-            //     breakpoint: 1500,
-            //     settings: {
-            //         arrows: false,
-            //         dots: true
-            //     }
-            // },
-            // {
-            //     breakpoint: 1000,
-            //     settings: {
-            //         slidesToShow: 1,
-            //         arrows: false,
-            //         dots: true
-            //     }
-            // },
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
         ]
     });
 
@@ -37,21 +28,20 @@ $(document).ready(() => {
         prevArrow: '<button type="button" class="slick-prev"><svg width="70" height="70" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg"><circle r="35" transform="matrix(-1 0 0 1 35 35)" fill="#F7F2EC"/><path d="M38 40.7485L36.8566 42L30.4615 35L36.8566 28L38 29.2515L32.7483 35L38 40.7485V40.7485Z" fill="black" fill-opacity="0.54"/></svg></button>',
         autoplay: true,
         responsive: [
-            // {
-            //     breakpoint: 1500,
-            //     settings: {
-            //         arrows: false,
-            //         dots: true
-            //     }
-            // },
-            // {
-            //     breakpoint: 1000,
-            //     settings: {
-            //         slidesToShow: 1,
-            //         arrows: false,
-            //         dots: true
-            //     }
-            // },
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
         ]
     });
 
@@ -60,6 +50,14 @@ $(document).ready(() => {
         arrows: false,
         dots: true,
         slidesToShow: 1,
-        autoplay: true,
+        autoplay: false,
+    });
+
+    /**
+     * HAMBURGER
+     */
+    $('.menu-toggle-inner').on('click', () => {
+        $('.adaptive-menu-toggle').toggleClass('adaptive-menu-toggle--open');
+        $('.headerNav_adaptive').toggleClass('header_opened');
     });
 });
